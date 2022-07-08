e7adaecf29bf38a7afadfa00e5b9cf0f906f5c9d
Jamesson Faria <jamessonjr@gmail.com>
2022-07-08 11:02:18 -0300

    Adding others unit tests

--- src/test/java/br/com/jamesson/RecipesControllerTest.java
+++ src/test/java/br/com/jamesson/RecipesControllerTest.java
@@ -7,17 +7,19 @@ import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @MicronautTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RecipesControllerTest {
 
     @Inject
@@ -25,6 +27,7 @@ public class RecipesControllerTest {
     HttpClient client;
 
     @Test
+    @Order(3)
     @DisplayName("Shoud GET recipes sucessfully")
     void shoudGetRecipesSuccessfully(){
         // given
@@ -32,13 +35,43 @@ public class RecipesControllerTest {
                 .header("Accept-Version", "1");
 
         // when
-        HttpResponse response = client.toBlocking()
+        HttpResponse<List<Recipe>> response = client.toBlocking()
                 .exchange(request, Argument.of(List.class, Recipe.class));
 
         // then
         assertEquals(HttpStatus.OK, response.getStatus());
-        assertEquals(3, ((ArrayList) response.body()).size());
+        assertEquals(4, response.body().size());
     }
 
+    @Test
+    @Order(2)
+    @DisplayName("Should POST recipes successfully")
+    void shouldPOSTRecipeSucessfully() {
+        // given
+        Recipe lasagna = new Recipe("Lasagna", 450, 1600);
+        HttpRequest request = HttpRequest.POST("/recipes", lasagna);
 
+        // when
+        HttpResponse<Recipe> response = client.toBlocking().exchange(request, Recipe.class);
+
+        // then
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(lasagna.getName(), response.body().getName());
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Should return error on invalid temp")
+    void shouldReturnErrorOnInvalidTemp() {
+        // given
+        Recipe lasagna = new Recipe("Lasagna", 1000, 1600);
+        HttpRequest request = HttpRequest.POST("/recipes", lasagna);
+
+        // when
+        HttpClientResponseException exception = assertThrows(HttpClientResponseException.class,
+                () -> client.toBlocking().exchange(request, Recipe.class));
+
+        // then
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    }
 }
